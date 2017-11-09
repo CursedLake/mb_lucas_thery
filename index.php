ae08d18c94bb3004d93b2c344f927db698c06c63
Lucas THERY <ltluc88@gmail.com>
2017-11-09 14:19:23 +0100

    Les cookies

---
+++ index.php
@@ -0,0 +1,116 @@
+<?php
+	include ("includes/connexion.inc.php");
+	include ("includes/haut.inc.php");
+	
+?>
+
+<!-- Header -->
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="intro-text">
+                        <span class="name">Le fil</span>
+                        <hr class="star-light">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- About Section -->
+    <section>
+        <div class="container">
+            <div class="row">
+				<?php 
+					if (isset($_GET['a']) && !empty($_GET['a'])) {
+							//	echo var_dump($_GET);
+
+						if($_GET['a'] == 'mod' ){
+							//echo 'modification...';
+							echo '<form method="GET" action="article.php">';
+							?>
+									<div class="col-sm-10">  
+										<div class="form-group">
+											<?php echo '<textarea id="message" name="message" class="form-control" placeholder="Message">'.$_GET['contenu'] .'</textarea>'; ?>
+											<!-- ajouter le label -->
+										</div>
+											<input type="hidden"  name="a"  value="mod">
+											<?php echo "<input type='hidden'  name='id'  value=" . $_GET['id'] .">" ?>
+
+									</div>
+									<div class="col-sm-2">
+										<button type="submit" class="btn btn-success btn-lg">Envoyer</button>
+									</div>                        
+								</form>
+						<?php
+						}
+					}
+					else{
+						echo '<form method="POST" action="message.php">';
+						?>
+									<div class="col-sm-10">  
+										<div class="form-group">
+											<textarea id="message" name="message" class="form-control" placeholder="Message"></textarea>
+											<!-- ajouter le label -->
+										</div>
+									</div>
+									<div class="col-sm-2">
+										<button type="submit" class="btn btn-success btn-lg">Envoyer</button>
+									</div>                        
+								</form>
+						<?php
+					}
+				?>
+                   
+            </div>
+			
+			<?php
+				$sql="SELECT * FROM messages ORDER BY date DESC";
+				$stmt=$pdo->query($sql);
+			?>
+						
+				<br /> <br />
+			<?php	
+			//}
+			?>
+
+            <div class="row">
+                <div class="col-md-12">
+				
+				<?php while ($data = $stmt->fetch()){
+				echo "<blockquote><p>" .$data['contenu'] . "</p><footer>" .date("F j, Y, g:i a",$data['date']) ."</footer></blockquote>";
+				echo '<a href="article.php?a=sup&id=' . $data['id'] . '", class="btn btn-danger">' . 'Supprimer</a>';
+				echo '<a href="index.php?a=mod&id=' . $data['id'] . '&contenu=' . $data['contenu'] .'", class="btn btn-warning">' . 'Modifier</a>';
+
+					//echo date ("d/m/Y H:i:s", /*timestamp - horodateur*/$data ['date']);
+				echo '<br /> <br />';
+				}
+				?>
+				
+				
+				<?php //echo $data['contenu']; echo $data['id']; ?>
+		
+                    <blockquote>
+                      <p>Lorem ipsum dolor sit amet, consectetur <a href="#">#adipiscing</a> elit. Integer posuere erat a ante.</p>
+                      <footer>Foo</footer>
+                    </blockquote>
+
+                    <blockquote>
+                      <p>Sed eu tellus vel lectus <a href="#">@rhoncus</a> maximus. Nam eu turpis ac eros pellentesque tincidunt. Maecenas pellentesque consequat libero</p>
+                      <footer>Mauris arcu</footer>
+                    </blockquote>
+
+                    <blockquote>
+                      <p>Nunc volutpat vel nibh vitae blandit</p>
+                      <footer>blandit</footer>
+                    </blockquote>
+                   
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php
+include ("includes/bas.inc.php");
+?>
---
+++ micro_blog.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  jeu. 19 oct. 2017 à 13:41
+-- Version du serveur :  5.7.19
+-- Version de PHP :  5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `micro_blog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+CREATE TABLE IF NOT EXISTS `messages` (
+  `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `contenu` varchar(255) NOT NULL,
+  `date` int(6) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `messages`
+--
+
+INSERT INTO `messages` (`id`, `contenu`, `date`) VALUES
+(22, 'blabla', 1506924575),
+(20, 'sdf12356', 1506680026),
+(19, 'dfs', 1506679934);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+DROP TABLE IF EXISTS `utilisateurs`;
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `email` varchar(25) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `sid` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
